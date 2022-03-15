b449ec8762c2ad344533630aa41a2fe433857d00
HomeChaos <ibi692009@yandex.ru>
2022-03-15 11:51:47 +0300

    add list of bad words

--- main.py
+++ main.py
@@ -9,19 +9,23 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
-# echo
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+@dp.message_handler(content_types=["new_chat_members"])
+async def new_user(message: types.Message):
+    first_name, last_name, username = ('', '', '')
+    for new_user in message.new_chat_members:
+        first_name = new_user.first_name
+        last_name = ' ' + new_user.last_name if new_user.last_name not in (None, 'None') else ''
+        username = ' ' + new_user.username if new_user.username not in (None, 'None') else ''
+    await message.answer(f'🍀 К нам присоединился космополит {first_name}{last_name}{username}!')
 
 
 # проверка плохих слов
-'''@dp.message_handler()
+@dp.message_handler()
 async def filter_messages(message: types.Message):
     for w in config.LIST_BAD_WORDS:
         if w in message.text.lower():
             await message.delete()
-            await message.answer(f'@{message.from_user.username} Ругаться нельзя!')'''
+            await message.answer(f'@{message.from_user.username} Ругаться нельзя!')
 
 
 # run long-polling
